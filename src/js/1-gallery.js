261156dfcbe6f74bc112e38c267852911cd76e86
Ira <dudarik86@gmail.com>
2024-09-03 14:36:16 -0600

    add index file

---
+++ src/js/1-gallery.js
@@ -0,0 +1,107 @@
+const images = [
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+];
+
+// Створюємо скрипт для створення галереї зображень на основі масиву даних.
+const gallery = document.querySelector('ul.gallery'); 
+
+
+const galleryMarkup = images.map(({ preview, original, description }) => {
+    
+   // Створюємо <li> елемент
+  const li = document.createElement('li'); 
+
+  // Створюємо <a> елемент
+  const link = document.createElement('a');
+  link.href = original;
+  link.classList.add('gallery_link');
+
+  // Створюємо <img> елемент
+  const img = document.createElement('img'); 
+  img.src = preview;
+  img.alt = description; 
+  img.dataset.source = original;
+  
+
+  link.appendChild(img); // Додаємо <img> в <a>
+  li.appendChild(link);
+  return li;
+});
+gallery.append(...galleryMarkup);
+
+
+
+gallery.addEventListener('click', function (event) {
+  event.preventDefault();
+ 
+  const target = event.target; // Отримуємо елемент, по якому був клік
+  
+  if (event.target.tagName === 'IMG') {  // Перевіряємо, що клік був саме по зображенню
+  
+    const largeImageURL = target.dataset.source;
+    // Отримуємо посилання на велике зображення з data-атрибуту
+      console.log(largeImageURL);
+      }
+});
